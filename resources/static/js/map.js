b21e6fe389c9fe5d919b68edb9d672671313cce4
lathoub <4082369+lathoub@users.noreply.github.com>
2021-09-07 10:33:46 +0200

    use https

--- resources/static/js/map.js
+++ resources/static/js/map.js
@@ -1,7 +1,7 @@
 // Leaf map
 let map = L.map('map').setView([0, 0], 12);
 
-let stapiBaseUrl = 'http://stapi.snuffeldb.synology.me/FROST-Server/v1.0'
+let stapiBaseUrl = 'https://stapi.snuffeldb.synology.me/FROST-Server/v1.0'
 
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
