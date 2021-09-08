3ba9e6ad8a667bf8b340ca876e60eec9d87d4cf6
lathoub <bart.de.lathouwer@telenet.be>
2021-09-08 09:24:33 +0200

    OSM tileLayer

--- resources/static/js/map.js
+++ resources/static/js/map.js
@@ -3,10 +3,8 @@ let map = L.map('map').setView([0, 0], 12);
 
 let stapiBaseUrl = 'https://stapi.snuffeldb.synology.me/FROST-Server/v1.0'
 
-L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-    subdomains: 'abcd',
-    maxZoom: 20
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // Get Location and Datastreams of all Things
