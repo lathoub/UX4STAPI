da4f96fd64fcfc5abfaefd64d3c2d58cf4adee34
RobinBenoot <r0678839@student.thomasmore.be>
2021-09-16 23:32:57 +0200

    Test sign in to index + sign up with modal

--- resources/static/js/login.js
+++ resources/static/js/login.js
@@ -1,51 +1,57 @@
-function validate(form){
-    let username = document.getElementById('inputUsername').value;
-    let password = document.getElementById('inputPassword').value;
-
-    if(username==="admin" && password==="admin"){
-        location.href= 'index.html';
-        return false;
-    }
-    else {
-        alert("Wrong credentials")
-        form.username.focus();
-    }
-    return true;
-
-}
-function toggleResetPswd(e){
+function toggleResetPswd(e) {
     e.preventDefault();
     $('#logreg-forms .form-signin').toggle() // display:block or none
     $('#logreg-forms .form-reset').toggle() // display:block or none
 }
 
-function toggleSignUp(e){
+function toggleSignUp(e) {
     e.preventDefault();
     $('#logreg-forms .form-signin').toggle(); // display:block or none
-    $('#logreg-forms .form-signup').toggle(); // display:block or none
 }
 
-$(()=>{
+$(() => {
     // Login Register Form
     $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
     $('#logreg-forms #cancel_reset').click(toggleResetPswd);
-    $('#logreg-forms #btn-signup').click(toggleSignUp);
-    $('#logreg-forms #cancel_signup').click(toggleSignUp);
 })
 
-const signupForm = document.querySelector('#signUpForm');
+// Sign up
+$('#btn-signupForm').click(function () {
+    $('#modal-signup').modal('hide');
+})
+const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
     // get user info
-    const email = signupForm['user-email'].value;
-    const password = signupForm['user-pass'].value;
+    const email = signupForm['signupEmail'].value;
+    const password = signupForm['signupPassword'].value;
 
+    console.log(email, password);
     // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
         console.log(cred.user);
     });
+    signupForm.reset();
 });
+
+// Sign in
+const loginForm = document.querySelector('#signInForm');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get user info
+    const email = loginForm['inputUsername'].value;
+    const password = loginForm['inputPassword'].value;
+
+    auth.signInWithEmailAndPassword(email,password).then(cred =>{
+        console.log(cred.user);
+        location.href= 'index.html';
+    })
+    loginForm.reset();
+})
+
+
+
 
 
 
