8bc128f1a3313f7dcfd7622e27d1191c62eaceb5
Robin Benoot <r0678839@student.thomasmore.be>
2021-09-18 18:53:09 +0200

    Sign in/sign out/User auth status

--- resources/static/js/login.js
+++ resources/static/js/login.js
@@ -15,6 +15,16 @@ $(() => {
     $('#logreg-forms #cancel_reset').click(toggleResetPswd);
 })
 
+// Listen to auth status changes
+    auth.onAuthStateChanged(user =>{
+        if (user){
+            console.log("User logged in: ", user)
+        } else {
+            location.href = 'login.html';
+            console.log("User logged out")
+        }
+    })
+
 // Sign up
 $('#btn-signupForm').click(function () {
     $('#modal-signup').modal('hide');
@@ -29,8 +39,7 @@ signupForm.addEventListener('submit', (e) => {
     console.log(email, password);
     // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
-    });
+    })
     signupForm.reset();
 });
 
@@ -43,12 +52,23 @@ loginForm.addEventListener('submit', (e) => {
     const email = loginForm['inputUsername'].value;
     const password = loginForm['inputPassword'].value;
 
-    auth.signInWithEmailAndPassword(email,password).then(cred =>{
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
         console.log(cred.user);
-        location.href= 'index.html';
+        location.href = 'index.html';
     })
     loginForm.reset();
+});
+
+// Sign out
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log("logged out")
+    })
+    location.href = 'login.html';
 })
+
 
 
 
